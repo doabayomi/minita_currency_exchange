83c1312f15d021ba73c889875b4960b899ba823d
Daniel Abayomi <dan234bayomi@gmail.com>
2024-09-03 12:47:24 +0100

    Currency api integration

--- currency.py
+++ currency.py
@@ -3,7 +3,7 @@ import requests
 import json
 
 
-def get_currency_list():
+def get_list():
     currency_list_url = "https://cdn.jsdelivr.net/npm/@fawazahmed0/" \
                         "currency-api@latest/v1/currencies.json"
     response = requests.get(currency_list_url)
@@ -17,4 +17,19 @@ def get_currency_list():
     return json.loads(response.json())
 
 
-def get_currency_prices()
+def get_latest_prices():
+    currency_price_url = "https://cdn.jsdelivr.net/npm/@fawazahmed0/" \
+                         "currency-api@latest/v1/currencies/eur.json"
+    response = requests.get(currency_price_url)
+
+    """Try the other URL if first URL is not working"""
+    if (response.status_code != 200):
+        currencies_list_url = "https://latest.currency-api.pages.dev/" \
+                              "v1/currencies/eur.json"
+        response = requests.get(currencies_list_url)
+
+    return json.loads(response.json())
+
+
+def get_prices_at(date):
+    pass
