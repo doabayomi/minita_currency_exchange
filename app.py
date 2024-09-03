a6943fab467f5e3fec51b29ec9a4c2597e9af4d9
Daniel Abayomi <dan234bayomi@gmail.com>
2024-09-03 12:50:16 +0100

    Added price list to app

--- app.py
+++ app.py
@@ -1,8 +1,12 @@
 from flask import Flask, render_template, url_for
+import currency
 
 app = Flask(__name__)
 
 
 @app.route("/")
 def index():
-    return render_template("index.html")
+    currency_list = currency.get_list()
+    latest_prices = currency.get_latest_prices()
+    return render_template("index.html", currency_list=currency_list,
+                           latest_prices=latest_prices)
