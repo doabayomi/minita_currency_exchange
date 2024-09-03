90205cb35febc7c603e134c98a33f99e29324600
Daniel Abayomi <dan234bayomi@gmail.com>
2024-09-03 12:41:48 +0100

    Creating currency functions

---
+++ currency.py
@@ -0,0 +1,20 @@
+#!/usr/bin/python3
+import requests
+import json
+
+
+def get_currency_list():
+    currency_list_url = "https://cdn.jsdelivr.net/npm/@fawazahmed0/" \
+                        "currency-api@latest/v1/currencies.json"
+    response = requests.get(currency_list_url)
+
+    """Try the other URL if first URL is not working"""
+    if (response.status_code != 200):
+        currencies_list_url = "https://latest.currency-api.pages.dev/" \
+                          "v1/currencies.json"
+        response = requests.get(currencies_list_url)
+
+    return json.loads(response.json())
+
+
+def get_currency_prices()
