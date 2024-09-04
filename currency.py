8fbe942021c5abe69fa26a6c43398bfa2f2067f9
Daniel Abayomi <dan234bayomi@gmail.com>
2024-09-04 07:04:14 +0100

    Added datetime functionality to currency api call

--- currency.py
+++ currency.py
@@ -1,5 +1,6 @@
 #!/usr/bin/python3
 import requests
+from datetime import date, timedelta
 
 
 def get_currency_list():
@@ -21,6 +22,32 @@ def get_currency_list():
     return response.json()
 
 
+def get_rates_at(target_date: date):
+    """Gets exchange rates at a specific date
+
+    Args:
+        target_date: Date to find target date
+
+    Returns:
+        JSON containing rates for each currency at target date
+    """
+    if target_date == date.today():
+        date_as_string = "latest"
+    else:
+        date_as_string = date.isoformat(target_date)
+
+    price_url = "https://cdn.jsdelivr.net/npm/@fawazahmed0/" \
+                f"currency-api@{date_as_string}/v1/currencies/eur.json"
+    fallback_price_url = f"https://{date_as_string}.currency-api.pages.dev/" \
+                         "v1/currencies/eur.json"
+
+    response = requests.get(price_url)
+    if response.status_code != 200:
+        response = requests.get(fallback_price_url)
+
+    return response.json()
+
+
 def get_latest_rates():
     """Get the latest prices for each currency based on EUR from
     currency api
@@ -28,22 +55,29 @@ def get_latest_rates():
     Returns:
         JSON containing code of currency and price.
     """
-    currency_price_url = "https://cdn.jsdelivr.net/npm/@fawazahmed0/" \
-                         "currency-api@latest/v1/currencies/eur.json"
-    response = requests.get(currency_price_url)
-
-    """Try the other URL if first URL is not working"""
-    if (response.status_code != 200):
-        currencies_list_url = "https://latest.currency-api.pages.dev/" \
-                              "v1/currencies/eur.json"
-        response = requests.get(currencies_list_url)
-
-    return response.json()
+    latest_rates = get_rates_at(date.today())
+    return latest_rates
 
 
 def convert_currency(currency_from, currency_to, amount):
-    pass
+    """Converts an amount from one currency to another
 
+    Args:
+        currency_from: Currency to convert from
+        currency_to: Currency to convert to
+        amount: Amount to be converted in initial currency
 
-def get_prices_at(date):
-    pass
+    Returns:
+        (float) Amount in target currency (currency_to)
+    """
+    current_rates = get_latest_rates()
+    currency_from_rate = current_rates[currency_from]
+    currency_to_rate = current_rates[currency_to]
+
+    """We convert it first to base currency (EUR) and then
+    the target currency we want
+    """
+    amount_in_euros = amount / currency_from_rate
+    amount_in_target_currency = amount_in_euros * currency_to_rate
+
+    return amount_in_target_currency
