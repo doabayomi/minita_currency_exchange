94f3986e761093eaf4c8a476d91b00bc57ddce8a
Daniel Abayomi <dan234bayomi@gmail.com>
2024-09-04 06:22:42 +0100

    Showing currency list

--- app.py
+++ app.py
@@ -1,4 +1,4 @@
-from flask import Flask, render_template, url_for
+from flask import Flask, render_template, request
 import currency
 
 app = Flask(__name__)
@@ -6,7 +6,6 @@ app = Flask(__name__)
 
 @app.route("/")
 def index():
-    currency_list = currency.get_list()
-    latest_prices = currency.get_latest_prices()
-    return render_template("index.html", currency_list=currency_list,
-                           latest_prices=latest_prices)
+    return render_template("index.html",
+                           currencies=currency.get_currency_list(),
+                           current_rates=currency.get_latest_rates())
--- currency.py
+++ currency.py
@@ -1,9 +1,13 @@
 #!/usr/bin/python3
 import requests
-import json
 
 
-def get_list():
+def get_currency_list():
+    """Gets the list of currencies from currency api
+
+    Returns:
+        JSON from api containing currency code and full name
+    """
     currency_list_url = "https://cdn.jsdelivr.net/npm/@fawazahmed0/" \
                         "currency-api@latest/v1/currencies.json"
     response = requests.get(currency_list_url)
@@ -11,13 +15,19 @@ def get_list():
     """Try the other URL if first URL is not working"""
     if (response.status_code != 200):
         currencies_list_url = "https://latest.currency-api.pages.dev/" \
-                          "v1/currencies.json"
+                              "v1/currencies.json"
         response = requests.get(currencies_list_url)
 
-    return json.loads(response.json())
+    return response.json()
 
 
-def get_latest_prices():
+def get_latest_rates():
+    """Get the latest prices for each currency based on EUR from
+    currency api
+
+    Returns:
+        JSON containing code of currency and price.
+    """
     currency_price_url = "https://cdn.jsdelivr.net/npm/@fawazahmed0/" \
                          "currency-api@latest/v1/currencies/eur.json"
     response = requests.get(currency_price_url)
@@ -28,7 +38,11 @@ def get_latest_prices():
                               "v1/currencies/eur.json"
         response = requests.get(currencies_list_url)
 
-    return json.loads(response.json())
+    return response.json()
+
+
+def convert_currency(currency_from, currency_to, amount):
+    pass
 
 
 def get_prices_at(date):
