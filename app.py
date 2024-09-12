f1214a9b15c62c43f2f32f2e38cf50bb7f50361b
Daniel Abayomi <dan234bayomi@gmail.com>
2024-09-12 08:15:53 +0100

    111

--- app.py
+++ app.py
@@ -1,8 +1,14 @@
-from flask import Flask, render_template, request
-import currency
+from flask import Flask, render_template, request, jsonify
+from currency_api import (
+    get_currency_list,
+    get_relative_rates_for,
+    export_data_to_chart,
+    convert_currency_at
+)
 import market
 from datetime import date
 import os
+import json
 
 app = Flask(__name__)
 app.config.from_object("config.Config")
@@ -14,12 +20,12 @@ def index():
     if request.method == 'GET':
         if request.args.get('amount') is None:
             return render_template("index.html",
-                                   currencies=currency.get_currency_list(),
+                                   currencies=get_currency_list(),
                                    input="empty")
         elif request.args.get('amount') == '':
             """Invalid input test case"""
             return render_template("index.html",
-                                   currencies=currency.get_currency_list(),
+                                   currencies=get_currency_list(),
                                    input="invalid")
         else:
             amount = request.args.get('amount')
@@ -30,15 +36,11 @@ def index():
             chart_data_path = os.path.join(app.static_folder,
                                            'data',
                                            'chart.json')
-            currency.export_data_to_chart(currency_from,
-                                          currency_to,
-                                          chart_data_path)
+            export_data_to_chart(currency_from, currency_to, chart_data_path)
 
             """Exporting conversion data"""
-            converted_amount = currency.convert_currency_at(currency_from,
-                                                            currency_to,
-                                                            amount,
-                                                            date.today())
+            converted_amount = convert_currency_at(currency_from, currency_to,
+                                                   amount, date.today())
             answer = {'amount': float(amount),
                       'currency_from': currency_from,
                       'currency_to': currency_to,
@@ -49,7 +51,22 @@ def index():
                                                          news_api_token)
 
             return render_template("index.html",
-                                   currencies=currency.get_currency_list(),
+                                   currencies=get_currency_list(),
                                    input="valid",
                                    result=answer,
                                    news=news_output['data'])
+
+
+@app.route("/api/prices/weekly/<currency_from>/<currency_to>")
+def get_weekly_price_data(currency_from, currency_to):
+    return jsonify(get_relative_rates_for(currency_from, currency_to, 7))
+
+
+@app.route("api/prices/monthly/<currency_from>/<currency_to>")
+def get_monthly_price_data(currency_from, currency_to):
+    return jsonify(get_relative_rates_for(currency_from, currency_to, 30))
+
+
+@app.route("api/prices/yearly/<currency_from>/<currency_to>")
+def get_monthly_price_data(currency_from, currency_to):
+    return jsonify(get_relative_rates_for(currency_from, currency_to, 365))
